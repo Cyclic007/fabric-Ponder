f40a51e371291baf3405a53e7c18842a9a6a21d5
Cyclic <cyclic007dev@gmail.com>
2024-05-06 10:40:29 -0500

    sanity check time

--- src/main/java/org/goodluck/fabric_ponder/FabricPonder.java
+++ src/main/java/org/goodluck/fabric_ponder/FabricPonder.java
@@ -1,2 +1,15 @@
-public class FabricPonder {
+package org.goodluck.fabric_ponder;
+
+import dev.latvian.mods.kubejs.fabric.KubeJSFabric;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FabricPonder extends KubeJSFabric implements DedicatedServerModInitializer {
+    private static final Logger log = LogManager.getLogger(FabricPonder.class);
+
+    @Override
+    public void onInitializeServer() {
+        log.info("hello server");
+    }
 }
--- src/main/java/org/goodluck/fabric_ponder/client/FabricPonderClient.java
+++ src/main/java/org/goodluck/fabric_ponder/client/FabricPonderClient.java
@@ -1,2 +1,15 @@
-public class FabricPonderClient {
+package org.goodluck.fabric_ponder.client;
+
+import dev.latvian.mods.kubejs.fabric.KubeJSFabricClient;
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FabricPonderClient extends KubeJSFabricClient implements ClientModInitializer {
+    private static final Logger log = LogManager.getLogger(FabricPonderClient.class);
+
+    @Override
+    public void onInitializeClient() {
+        log.info("hello client");
+    }
 }
